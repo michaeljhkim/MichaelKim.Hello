fd4d0ed16673cc9b4a2fca87b56101c04f31ec4b
michaeljhkim <michaelkim291@gmail.com>
2025-05-06 19:43:45 +0000

    finished sql setup for the most part - have not tested yet

--- michaelkim.hello.backend/michaelkim.hello.backend.ApiService/Program.cs
+++ michaelkim.hello.backend/michaelkim.hello.backend.ApiService/Program.cs
@@ -1,5 +1,9 @@
+// aspire already handles the base projects "using" calls, but any other packages need to explicitly be called
+using Npgsql; 
+
 var builder = WebApplication.CreateBuilder(args);
 builder.AddServiceDefaults();
+builder.AddNpgsqlDataSource(connectionName: "hello_db");
 
 // Add CORS service and specify allowed origins - need to directly note the address due to extension policy
 builder.Services.AddCors(options => {
@@ -30,5 +34,33 @@ app.MapDefaultEndpoints();
 // Hello World endpoint
 app.MapGet("/helloworld", () => Results.Ok("Hello World from .NET!"))
    .WithName("GetHelloWorld");
+
+
+// --- TEST QUERYING ENDPOINTS ---
+app.MapGet("/hello_information1", async (NpgsqlDataSource dataSource) => {
+    await using var connection = await dataSource.OpenConnectionAsync();
+    await using var cmd = new NpgsqlCommand("SELECT id, name FROM items", connection);
+    await using var reader = await cmd.ExecuteReaderAsync();
+
+    var items = new List<Dictionary<string, object>>();
+    while (await reader.ReadAsync()) {
+        var item = new Dictionary<string, object> {
+            { "Id", reader.GetInt32(0) },
+            { "Name", reader.GetString(1) }
+        };
+        items.Add(item);
+    }
+
+    return Results.Ok(items);
+});
+
+// From nuget: https://www.nuget.org/packages/Npgsql.DependencyInjection
+app.MapGet("/hello_information2", async (NpgsqlConnection connection) => {
+    await connection.OpenAsync();
+    await using var command = new NpgsqlCommand("SELECT number FROM data LIMIT 1", connection);
+    return "Hello World: " + await command.ExecuteScalarAsync();
+});
+
+
 
 app.Run();
