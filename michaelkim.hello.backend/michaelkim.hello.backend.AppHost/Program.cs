ae08015885ffa15d3a9e07f1fb367203c77b835d
michaeljhkim <michaelkim291@gmail.com>
2025-05-07 22:02:36 +0000

    added back modified creationScript

--- michaelkim.hello.backend/michaelkim.hello.backend.AppHost/Program.cs
+++ michaelkim.hello.backend/michaelkim.hello.backend.AppHost/Program.cs
@@ -1,7 +1,34 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+/*
+Create postgresql server. Added default values for testing.
+*/
 var postgres = builder.AddPostgres("postgres");
-var postgresdb = postgres.AddDatabase("hellodb");
+
+var databaseName = "hellodb";
+var creationScript = $$"""
+    -- Create the database
+    CREATE DATABASE {{databaseName}};
+
+    -- Create the table
+    CREATE TABLE hello_information (
+        person_id INT PRIMARY KEY,
+        first_name VARCHAR(50),
+        last_name VARCHAR(50),
+        age INT,
+        email VARCHAR(50),
+        github VARCHAR(50),
+        birth_date DATE
+    );
+
+    INSERT INTO hello_information (person_id, first_name, last_name, age, email, github, birth_date)
+    VALUES (0, 'Michael', 'Kim', '21', 'michaelkimwork47@gmail.com', https://github.com/michaeljhkim, '2003-09-08');
+
+""";
+
+var postgresdb = postgres.AddDatabase(databaseName)
+    .WithCreationScript(creationScript);
+
 
 var apiService = builder.AddProject<Projects.michaelkim_hello_backend_ApiService>("MKapiservice")
     //.WithHttpsHealthCheck("/health")
